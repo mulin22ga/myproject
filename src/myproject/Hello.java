9b720516497c5b368c9e905d44e22b2478d8549e
Anson <happy.wang992@gmail.com>
2017-10-24 15:35:10 +0800

    Scanner

--- src/myproject/Hello.java
+++ src/myproject/Hello.java
@@ -1,5 +1,7 @@
 package myproject;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
@@ -7,10 +9,17 @@ public class Hello {
 		System.out.println("Hello World");
 		Person p = new Person();
 		p.sayHello();
+		//
+		System.out.println("請輸入您的名字");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		System.out.println("Hello," +line);
 		
-		
-		
-		
+		System.out.println("你幾歲?");
+		line = scanner.nextLine();
+		int age = Integer.parseInt(line);
+		System.out.println(line);
+		System.out.println(age >25);
 		
 		
 	}
---
+++ src/myproject/VarribleTester.java
@@ -0,0 +1,37 @@
+package myproject;
+
+import java.util.Random;
+
+public class VarribleTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 0;
+		float weight = 69.5f;
+		char c = 65;
+		char c2 = 'Z';
+		int i = c2;
+		System.out.println(i);
+		boolean pass = false;
+		System.out.println(!pass);
+	    System.out.println(pass);
+	    
+	    String s = new String("Hello");
+	    Random r = new Random();
+	    int n1 = r.nextInt(6)+1;
+	    int n2 = r.nextInt(6)+1;
+	    int n3 = r.nextInt(6)+1;
+	    int n4 = r.nextInt(6)+1;
+	    System.out.println(3 <= 2);
+	    
+	    int len = s.length();
+	    System.out.println("len:" + len);
+	    System.out.println(s.charAt(3));
+	    
+
+	    
+	    
+	    
+	}
+
+}
