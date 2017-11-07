c53373214ae2dd2d954db74e7dfb99ce3eb0d34a
Anson <happy.wang992@gmail.com>
2017-11-07 15:56:45 +0800

    Prime

--- src/myproject/Niner22222.java
+++ src/myproject/Niner22222.java
@@ -5,12 +5,15 @@ public class Niner22222 {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 2; j <= 5; j++) {
-				if (i * j < 10) {
+				if (i * j /10<1) {
 
+					System.out.print(j + " X " + i + " =  " + (i * j) + "\t");
+				}else{
 					System.out.print(j + " X " + i + " = " + (i * j) + "\t");
 				}
 			}
 			System.out.println();
+			
 		}
 
 	}
