7fe33f13dc86c9656bfe72bf12b5dcd8781f1450
mulin22ga <happy.wang992@gmail.com>
2017-12-11 15:18:23 +0800

    改星星

--- src/myproject/Stairs.java
+++ src/myproject/Stairs.java
@@ -8,10 +8,15 @@ public class Stairs {
 		System.out.println("請使用者輸入一個數字");
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
-		int age = Integer.parseInt(line);
-		for (int n=1; n <=age; n++) {
-System.out.println("*");
-
+		int n = Integer.parseInt(line);
+		for (int a = 1; a <= n; a++) {
+			for (int i = 1; i <= a; i++) {
+				System.out.print("*");
+			}
+			for (int u = 1; u <= (n - a); u++) {
+				System.out.print("#");
+			}
+			System.out.println("");
 		}
 	}
 
