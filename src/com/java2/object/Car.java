d5c123aed7c6b404ede818bfe478c8c15a9469c9
mulin22ga <happy.wang992@gmail.com>
2018-03-11 00:32:02 +0800

    Java2

--- src/com/java2/object/Car.java
+++ src/com/java2/object/Car.java
@@ -1,22 +1,19 @@
 package com.java2.object;
 
-import java.util.Scanner;
-
 public class Car {
+private double discount;
+String CarName;
+int price;
 
-	public static void main(String[] args) {
-		System.out.println("請輸入想購買車種");
-		System.out.println("1.Benz");
-		System.out.println("2.BMW");
-		Scanner scanner = new Scanner(System.in);
-		String line = scanner.nextLine();
-		int m = Integer.parseInt(line);
-		if (m == 1) {
-			Car2 c1 = new Car2(1000);
-		}
-		if (m == 2) {
-			System.out.println("2222");
-		}
-	}
-
+public Car (String CarName,int price,double discount) {
+	this.CarName=CarName;
+	this.price=price;
+	this.discount=discount;
+}
+public void print() {
+	System.out.println(CarName+"\t"+price+"\t"+discount+"\t"+getTotalPrice());
+}
+public int getTotalPrice() {
+	return (int) (price*discount);
+}
 }
---
+++ src/com/java2/object/Tester.java
@@ -0,0 +1,13 @@
+package com.java2.object;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		Car c  = new Car("Benz", 1000, 0.9);
+		Car c2 = new Car("BMW", 1000, 0.8);
+		c.print();
+		c2.print();
+	}
+
+}
