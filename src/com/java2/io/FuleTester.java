8de3151f4e31cbaea62d43fbd1e27f74bdf18168
mulin22ga <happy.wang992@gmail.com>
2018-04-10 16:27:00 +0800

    FileTester

---
+++ src/com/java2/io/FuleTester.java
@@ -0,0 +1,22 @@
+package com.java2.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FuleTester {
+
+	public static void main(String[] args) {
+		try {
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found!");
+		} catch (IOException e) {
+			System.out.println("File Output problem");
+		}
+
+	}
+}
