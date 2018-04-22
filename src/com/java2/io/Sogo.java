7eaee7e8797723e1ef23d770a4868a0f56b3ef27
mulin22ga <happy.wang992@gmail.com>
2018-04-22 23:46:24 +0800

    改個

--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -3,29 +3,60 @@ package com.java2.io;
 import java.nio.channels.ShutdownChannelGroupException;
 import java.util.Scanner;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.channels.ShutdownChannelGroupException;
+import java.util.Scanner;
+
 public class Sogo {
+	Scanner scanner = new Scanner(System.in);
+
 	public Sogo() {
 
 	}
 
-	public void star() {
+	public void start() {
 		int function = 0;
-		showFunctions();
-		Scanner scanner = new Scanner(System.in);
 		while (function != 3) {
+			showFunctions();
 			function = scanner.nextInt();
 			switch (function) {
+			case 1:
+				inputSales();
+				break;
 			case 3:
 				return;
 			}
 		}
 	}
 
-	private static void showFunctions() {
-		System.out.println("請輸入功能(1~3):");
-		System.out.println("1)請輸入銷售紀錄");
-		System.out.println("2)印出銷售報表");
-		System.out.println("3)結束程式");
+	public void inputSales() {
+		try {
+			FileOutputStream fos = new FileOutputStream("sales.txt", true);
+			PrintStream out = new PrintStream(fos);
+			System.out.print("請輸入會員等級:");
+			int type = scanner.nextInt();
+			System.out.print("請輸入銷售金額:");
+			int amount = scanner.nextInt();
+			out.println(type + "\t" + amount);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
+	public void showFunctions() {
+		System.out.println("請輸入功能(1~3):");
+		System.out.println("1) 輸入銷售記錄");
+		System.out.println("2) 印出銷售報表");
+		System.out.println("3) 結束程式");
+	}
+
+	public static void main(String[] args) {
+		Sogo sogo = new Sogo();
+		sogo.start();
+	}
 }
