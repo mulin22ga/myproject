10b17b0850401aa49d365ab80f2c48b492aa3751
mulin22ga <happy.wang992@gmail.com>
2018-05-22 16:32:21 +0800

    snow

---
+++ src/com/java2/snowing/SnowFrame.java
@@ -0,0 +1,15 @@
+package com.java2.snowing;
+
+import javax.crypto.SecretKeyFactorySpi;
+import javax.swing.JFrame;
+
+public class SnowFrame extends JFrame {
+
+	public SnowFrame() {
+		setSize(620, 850);
+		setLocation(200, 200);
+	
+	
+	}
+
+}
